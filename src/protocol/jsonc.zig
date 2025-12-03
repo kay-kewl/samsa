32df22368d10c39965d3f3fcfeaaba05a4476dc0
Kirill Komov <dkaykewlp@gmail.com>
2025-12-03 10:43:29 +0300

    chore(build): fix jsonc std.ArrayList allocation

--- src/protocol/jsonc.zig
+++ src/protocol/jsonc.zig
@@ -1,8 +1,11 @@
 const std = @import("std");
 
 pub fn stripJsonc(allocator: std.mem.Allocator, input: []const u8) ![]u8 {
-    var out = std.ArrayList(u8).init(allocator);
-    errdefer out.deinit();
+    var out = std.ArrayList(u8){};
+    defer out.deinit(allocator);
+
+    try out.ensureTotalCapacity(allocator, input.len);
+
     var i: usize = 0;
     var in_string = false;
     while (i < input.len) : (i += 1) {
@@ -12,11 +15,31 @@ pub fn stripJsonc(allocator: std.mem.Allocator, input: []const u8) ![]u8 {
         }
 
         if (!in_string and c == '/' and i + 1 < input.len and input[i + 1] == '/') {
-            while (i < input.len and input[i] != '\n') : (i += 1) {}
+            while (i + 1 < input.len and input[i + 1] != '\n') : (i += 1) {}
         } else {
-            try out.append(c);
+            try out.append(allocator, c);
         }
     }
 
-    return out.toOwnedSlice();
+    return out.toOwnedSlice(allocator);
+}
+
+test "stripJsonc: removes comments" {
+    const input =
+        \\{
+        \\  "key": "value", // comment
+        \\  "url": "https://example.com"
+        \\}
+    ;
+
+    const expected =
+        \\{
+        \\  "key": "value", 
+        \\  "url": "https://example.com"
+        \\}
+    ;
+
+    const output = try stripJsonc(std.testing.allocator, input);
+    defer std.testing.allocator.free(output);
+    try std.testing.expectEqualSlices(u8, expected, output);
 }
