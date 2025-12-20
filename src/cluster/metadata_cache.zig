9c7141765f8a141af014e9a416f508b7234d258a
Kirill Komov <dkaykewlp@gmail.com>
2025-12-20 14:12:45 +0300

    feat(cluster): implement metadata cache from generated Metadata response

---
+++ src/cluster/metadata_cache.zig
@@ -0,0 +1,68 @@
+const std = @import("std");
+const metadata = @import("../generated/metadata.zig");
+const model = @import("model.zig");
+
+pub const Cache = struct {
+    allocator: std.mem.Allocator,
+    brokers: std.AutoHashMap(i32, model.Broker),
+    leaders: std.StringHashMap(std.AutoHashMap(i32, i32)),
+
+    pub fn init(allocator: std.mem.Allocator) Cache {
+        return .{
+            .allocator = allocator,
+            .brokers = std.AutoHashMap(i32, model.Broker).init(allocator),
+            .leaders = std.StringHashMap(std.AutoHashMap(i32, i32)).init(allocator),
+        };
+    }
+
+    pub fn deinit(self: *Cache) void {
+        var it = self.leaders.iterator();
+        while (it.next()) |entry| {
+            entry.value_ptr.deinit();
+            self.allocator.free(entry.key_ptr.*);
+        }
+
+        self.leaders.deinit();
+        self.brokers.deinit();
+    }
+
+    pub fn clear(self: *Cache) void {
+        var it = self.leaders.iterator();
+        while (it.next()) |entry| {
+            entry.value_ptr.deinit();
+            self.allocator.free(entry.key_ptr.*);
+        }
+
+        self.leaders.clearRetainingCapacity();
+        self.brokers.clearRetainingCapacity();
+    }
+
+    pub fn apply(self: *Cache, response: metadata.Response) !void {
+        self.clear();
+
+        for (response.brokers) |b| {
+            if (b.port <= 0 and b.port > std.math.maxInt(u16)) {
+                continue;
+            }
+
+            try self.brokers.put(b.node_id, .{
+                .node_id = b.node_id,
+                .host = b.host,
+                .port = @intCast(b.port),
+            });
+        }
+
+        for (response.topics) |t| {
+            const topic_name = t.name orelse continue;
+            const name_copy = try self.allocator.dupe(u8, topic_name);
+            var part_map = std.AutoHashMap(i32, i32).init(self.allocator);
+            errdefer part_map.deinit();
+
+            for (t.partitions) |p| {
+                try part_map.put(p.partition_index, p.leader_id);
+            }
+
+            try self.leaders.put(name_copy, part_map);
+        }
+    }
+};
