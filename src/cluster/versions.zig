736b1d5110d58f8ec6eac16f1f797c0b832b7ba1
Kirill Komov <dkaykewlp@gmail.com>
2025-12-20 12:58:11 +0300

    feat(cluster): implement negotiated API versions registry

---
+++ src/cluster/versions.zig
@@ -0,0 +1,44 @@
+const std = @import("std");
+const types = @import("../protocol//types.zig");
+const api_versions = @import("../generated/api_versions.zig");
+const errors = @import("errors.zig");
+
+pub const Range = struct {
+    min: i16,
+    max: i16,
+};
+
+pub const Registry = struct {
+    by_api_key: std.AutoHashMap(i16, Range),
+
+    pub fn init(allocator: std.mem.Allocator) Registry {
+        return .{
+            .by_api_key = std.AutoHashMap(i16, Range).init(allocator),
+        };
+    }
+
+    pub fn deinit(self: *Registry) void {
+        self.by_api_key.deinit();
+    }
+
+    pub fn updateFromApiVersions(self: *Registry, response: api_versions.Response) !void {
+        self.by_api_key.clearRetainingCapacity();
+        for (response.api_keys) |k| {
+            try self.by_api_key.put(k.api_key, .{
+                .min = k.min_version,
+                .max = k.max_version,
+            });
+        }
+    }
+
+    pub fn choose(self: *const Registry, api_key: types.ApiKey, preferred: i16) errors.ClusterError!i16 {
+        const range = self.by_api_key.get(@intFromEnum(api_key)) orelse return error.VersionNotNegotiated;
+        if (preferred < range.min) {
+            return range.min;
+        } else if (preferred > range.max) {
+            return range.max;
+        }
+
+        return preferred;
+    }
+};
